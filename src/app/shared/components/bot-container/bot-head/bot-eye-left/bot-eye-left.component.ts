4b63f0ca43e4fe8d12c8c6e098b822aa2795f4fd
Nathan Botelho <nimbotelho@gmail.com>
2020-07-15 01:48:33 -0300

    refactor: general hostlistener environment clean up

--- src/app/shared/components/bot-container/bot-head/bot-eye-left/bot-eye-left.component.ts
+++ src/app/shared/components/bot-container/bot-head/bot-eye-left/bot-eye-left.component.ts
@@ -1,36 +1,17 @@
-import { Component, OnInit, Input, OnDestroy, ViewChild, ElementRef } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { BotCommand } from 'src/app/shared/interfaces/bot-command';
-import { EventService } from 'src/app/core/services/event.service';
-import { Subscription, interval } from 'rxjs';
-import { debounce } from 'rxjs/operators';
 
 @Component({
   selector: 'app-bot-eye-left',
   templateUrl: './bot-eye-left.component.html',
   styleUrls: ['./bot-eye-left.component.scss']
 })
-export class BotEyeLeftComponent implements OnInit, OnDestroy {
-
-  @ViewChild('eyeLeft') element: ElementRef;
+export class BotEyeLeftComponent implements OnInit {
 
   @Input() command: BotCommand;
 
-  subscription: Subscription;
+  constructor() { }
 
-  constructor(private eventService: EventService) { }
-
-  ngOnInit(): void {
-    // this.subscription = this.eventService.onMouseMove
-    // .subscribe(() => this.command.inputPosition(this.eventService.percentConfig));
-
-
-    console.log(this.element);
-
-
-  }
-
-  ngOnDestroy(): void {
-    this.subscription.unsubscribe();
-  }
+  ngOnInit(): void { }
 
 }
--- src/app/shared/components/bot-container/bot-head/bot-head.component.ts
+++ src/app/shared/components/bot-container/bot-head/bot-head.component.ts
@@ -1,17 +1,28 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, OnDestroy, Input } from '@angular/core';
 import { BotCommand } from 'src/app/shared/interfaces/bot-command';
+import { EventService } from 'src/app/core/services/event.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-bot-head',
   templateUrl: './bot-head.component.html',
   styleUrls: ['./bot-head.component.scss']
 })
-export class BotHeadComponent implements OnInit {
+export class BotHeadComponent implements OnInit, OnDestroy {
 
   @Input() command: BotCommand;
 
-  constructor() { }
+  subscription: Subscription;
 
-  ngOnInit(): void { }
+  constructor(private eventService: EventService) { }
+
+  ngOnInit(): void {
+    this.subscription = this.eventService.onMouseMove
+      .subscribe(() => this.command.inputMousePosition(this.eventService.percentConfig));
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
 
 }
