3138590e6599db417e05c6c4c5e3dcb36711db2e
Nathan Botelho <nimbotelho@gmail.com>
2021-02-27 21:39:40 -0300

    feat: updating chat command factory instances

--- src/app/shared/factories/chat-command-factory.ts
+++ src/app/shared/factories/chat-command-factory.ts
@@ -1,10 +1,14 @@
 import { ChatCommandList } from 'src/app/shared/interfaces/chat-types';
+import { ChatStateDanger } from 'src/app/shared/models/chat-states/chat-danger';
+import { ChatStateStandard } from 'src/app/shared/models/chat-states/chat-standard';
+import { ChatStateRainbow } from 'src/app/shared/models/chat-states/chat-rainbow';
+import { ChatStateWarning } from 'src/app/shared/models/chat-states/chat-warning';
 
 export function chatCommandFactory(): ChatCommandList {
     return {
-        CHANGE_BALLOON_DANGER: 'ChatCommand' as any,
-        CHANGE_BALLOON_STANDARD: 'ChatCommand' as any,
-        CHANGE_BALLOON_RAINBOW: 'ChatCommand' as any,
-        CHANGE_BALLOON_WARNING: 'ChatCommand' as any,
+        CHANGE_BALLOON_DANGER: new ChatStateDanger(),
+        CHANGE_BALLOON_STANDARD: new ChatStateStandard(),
+        CHANGE_BALLOON_RAINBOW: new ChatStateRainbow(),
+        CHANGE_BALLOON_WARNING: new ChatStateWarning(),
     };
 }
