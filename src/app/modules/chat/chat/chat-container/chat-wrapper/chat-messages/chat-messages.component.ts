44f125fb2b06ad5bbd695ce2c77ba02c060c67a9
Nathan Botelho <nimbotelho@gmail.com>
2021-03-02 21:58:37 -0300

    feat: add smooth scroll to chat messages

--- src/app/modules/chat/chat/chat-container/chat-wrapper/chat-messages/chat-messages.component.ts
+++ src/app/modules/chat/chat/chat-container/chat-wrapper/chat-messages/chat-messages.component.ts
@@ -1,17 +1,65 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, OnDestroy, Input, ViewChild, ElementRef } from '@angular/core';
+import { CommandService } from 'src/app/core/services/command.service';
+import { ChatService } from 'src/app/modules/chat/services/chat.service';
+import { ChatMessage } from 'src/app/shared/models/chat/chat-message';
 import { ChatBalloon } from 'src/app/shared/interfaces/chat-balloon';
+import { ChatCommand } from 'src/app/shared/interfaces/chat-types';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-chat-messages',
   templateUrl: './chat-messages.component.html',
   styleUrls: ['./chat-messages.component.scss']
 })
-export class ChatMessagesComponent implements OnInit {
+export class ChatMessagesComponent implements OnInit, OnDestroy {
 
-  @Input() balloons: ChatBalloon[];
+  @ViewChild('chat', { static: true }) chat: ElementRef;
 
-  constructor() { }
+  private receiveSubscription: Subscription;
+  private sendSubscription: Subscription;
+  private errorSubscription: Subscription;
 
-  ngOnInit(): void { }
+  public balloons: ChatBalloon[];
+
+  constructor(
+    private chatService: ChatService,
+    private commandService: CommandService,
+  ) {
+    this.balloons = [];
+  }
+
+  public ngOnInit(): void {
+    this.sendSubscription = this.chatService.onMessage
+      .subscribe((message: ChatMessage) => this.handleNewChatMessage(message));
+    this.errorSubscription = this.chatService.onError
+      .subscribe((error: Error) => this.handleChatError(error));
+  }
+
+  public handleNewChatMessage(message: ChatMessage): void {
+    const key = this.commandService.resolveCommand('chat', message.content);
+    const command = this.commandService.resolveInstance(key).instance as ChatCommand;
+    this.balloons.push({ message, command, });
+    this.scrollToBottom();
+  }
+
+  public handleChatError(error: Error): void {
+    console.log('error');
+  }
+
+  public scrollToBottom(): void {
+    const element = this.chat.nativeElement;
+    const left = 0;
+    const top = element.scrollHeight;
+    const behavior = 'smooth';
+    setTimeout(() => element.scroll({ top, behavior, left }), 100);
+  }
+
+  public ngOnDestroy(): void {
+    if (this.receiveSubscription || this.sendSubscription || this.errorSubscription) {
+      this.receiveSubscription.unsubscribe();
+      this.sendSubscription.unsubscribe();
+      this.errorSubscription.unsubscribe();
+    }
+  }
 
 }
--- src/app/modules/chat/chat/chat-container/chat-wrapper/chat-wrapper.component.ts
+++ src/app/modules/chat/chat/chat-container/chat-wrapper/chat-wrapper.component.ts
@@ -1,58 +1,14 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
-import { CommandService } from 'src/app/core/services/command.service';
-import { ChatService } from 'src/app/modules/chat/services/chat.service';
-import { ChatMessage } from 'src/app/shared/models/chat/chat-message';
-import { ChatBalloon } from 'src/app/shared/interfaces/chat-balloon';
-import { Subscription } from 'rxjs';
-import { ChatCommand } from 'src/app/shared/interfaces/chat-types';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
     selector: 'app-chat-wrapper',
     templateUrl: './chat-wrapper.component.html',
     styleUrls: ['./chat-wrapper.component.scss']
 })
-export class ChatWrapperComponent implements OnInit, OnDestroy {
+export class ChatWrapperComponent implements OnInit {
 
-    public balloons: ChatBalloon[];
+    constructor() { }
 
-    private receiveSubscription: Subscription;
-    private sendSubscription: Subscription;
-    private errorSubscription: Subscription;
-
-    constructor(
-        private chatService: ChatService,
-        private commandService: CommandService,
-    ) {
-        this.balloons = [];
-    }
-
-    public ngOnInit(): void {
-        this.sendSubscription = this.chatService.onMessage
-            .subscribe((message: ChatMessage) => this.handleNewChatMessage(message));
-        this.errorSubscription = this.chatService.onError
-            .subscribe((error: Error) => this.handleChatError(error));
-    }
-
-    public handleNewChatMessage(message: ChatMessage): void {
-        const key = this.commandService.resolveCommand('chat', message.content);
-        const command = this.commandService.resolveInstance(key).instance as ChatCommand;
-
-        this.balloons.push({
-            message,
-            command,
-        });
-    }
-
-    public handleChatError(error: Error): void {
-        console.log('error');
-    }
-
-    public ngOnDestroy(): void {
-        if (this.receiveSubscription || this.sendSubscription || this.errorSubscription) {
-            this.receiveSubscription.unsubscribe();
-            this.sendSubscription.unsubscribe();
-            this.errorSubscription.unsubscribe();
-        }
-    }
+    public ngOnInit(): void { }
 
 }
