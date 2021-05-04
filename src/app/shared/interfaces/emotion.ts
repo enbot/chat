7b271bf5d1cd3eaa6bd2ff8a0726aee9f426d511
Nathan Botelho <nimbotelho@gmail.com>
2021-05-04 18:47:22 -0300

    feat: add integration emotion protocols

---
+++ src/app/shared/interfaces/emotion.ts
@@ -0,0 +1,14 @@
+export interface EmotionCategorization {
+    anger: number;
+    fear: number;
+    joy: number;
+    love: number;
+    sadness: number;
+    surprise: number;
+}
+
+export interface EmotionClassification {
+    negative: number;
+    neutral: number;
+    positive: number;
+}
