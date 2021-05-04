fb3e42b27fe277256ca53bce05b0a417b87979e8
Nathan Botelho <nimbotelho@gmail.com>
2021-05-04 18:46:16 -0300

    feat: add chat requests protocols

--- src/app/shared/interfaces/chat-requests.ts
+++ src/app/shared/interfaces/chat-requests.ts
@@ -1,16 +1,41 @@
-export interface SerializedEmotionDTO {
+import { EmotionCategorization, EmotionClassification } from './emotion';
+
+export interface EmotionRequestDTO {
     message: string;
 }
 
-export interface SerializedMessageDTO {
+export interface EmotionResponseDTO {
+    data: {
+        categorization: EmotionCategorization;
+        classification: EmotionClassification;
+    };
+    success: boolean;
+}
+
+export interface CalculateRequestDTO {
+    new: EmotionCategorization;
+    current: EmotionCategorization;
+}
+
+export interface CalculateResponseDTO {
+    data: {
+        categorization: EmotionCategorization;
+        emotion: {
+            amount: number;
+            name: keyof EmotionCategorization;
+        }
+    };
+    success: boolean;
+}
+
+export interface MessageRequestDTO {
     message: string;
     emotion: string;
 }
 
-export interface ResponseEmotionDTO {
-    emotion: string;
-}
-
-export interface ResponseMessageDTO {
-    message: string;
+export interface MessageResponseDTO {
+    data: {
+        response: string;
+    };
+    success: boolean;
 }
